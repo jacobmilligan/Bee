ed52f449bbf3eac6ff9707e243aeb771e3305e34
Jacob Milligan <jacobpmilligan@gmail.com>
2020-01-01 16:13:16 +1100

    refactor/Core: remove deprecated serialization and reflection tests

---
+++ Source/Runtime/Bee/Core/Tests/ReflectionTests.cpp
@@ -0,0 +1,22 @@
+/*
+ *  ReflectionTestsV2.cpp
+ *  Bee
+ *
+ *  Copyright (c) 2019 Jacob Milligan. All rights reserved.
+ */
+
+#include <Bee/Core/Reflection.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(ReflectionTestsV2, templated_types)
+{
+    const auto char_array_type = bee::get_type<bee::DynamicArray<char>>();
+    ASSERT_EQ(char_array_type->size, sizeof(bee::DynamicArray<char>));
+    ASSERT_EQ(char_array_type->alignment, alignof(bee::DynamicArray<char>));
+
+    using nested_t = bee::DynamicArray<bee::DynamicArray<bee::DynamicArray<char>>>;
+    const auto nested_array_type = bee::get_type<nested_t>();
+    ASSERT_EQ(nested_array_type->size, sizeof(nested_t));
+    ASSERT_EQ(nested_array_type->alignment, alignof(nested_t));
+}
