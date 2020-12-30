0628a7cf911a3c701541bf4855d3394b0b8504e3
Jacob Milligan <jacobpmilligan@gmail.com>
2020-12-31 03:12:25 +1100

    ThirdParty: add script for cleanly removing a git submodule

---
+++ ThirdParty/remove-submodule.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if "%1"=="" (
+    echo usage: remove-submodule.bat ^<submodule^>
+    exit /b 0
+)
+
+pushd ..
+
+git submodule deinit -f -- ThirdParty\%1
+rmdir /s .git\modules\ThirdParty\%1
+git rm -f ThirdParty\%1
+
+exit /b 0
